0d11ca2f8d1c2ddce9907abe56a2566cb7951313
chengzr01 <ziruicheng01@gmail.com>
2023-01-14 20:47:47 +0800

    deploy

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ function App() {
         <h1> Zirui Cheng 程子睿</h1>
         <h3>Department of Computer Science and Technology, Tsinghua University </h3>
         <h3>chengzr19@mails.tsinghua.edu.cn</h3>
-        <h3>Beijing, China</h3>
       </header>
     </div>
   );
