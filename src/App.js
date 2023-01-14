4aff4eca1538e877a6dae7a577b249c317161e6c
chengzr01 <ziruicheng01@gmail.com>
2023-01-14 20:37:54 +0800

    init

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1> Zirui Cheng 程子睿</h1>
+        <h3>Department of Computer Science and Technology, Tsinghua University </h3>
+        <h3>chengzr19@mails.tsinghua.edu.cn</h3>
+      </header>
+    </div>
+  );
+}
+
+export default App;
