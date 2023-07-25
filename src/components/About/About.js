17a54b38f40247ddb1c572b1883848765f0bec4c
chengzr01 <ziruicheng01@gmail.com>
2023-07-24 22:09:17 -0400

    add pictures

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Container, Row, Col, Card } from "react-bootstrap";
 import Zurich from "../../Assets/Misc/Zurich.jpg";
 import Luzern from "../../Assets/Misc/Luzern.jpg";
+import Interlaken from "../../Assets/Misc/Interlaken.jpg";
 
 function About() {
   return (
@@ -20,35 +21,45 @@ function About() {
             </i>{" "}
           </p>
           <p style={{ textAlign: "right" }}>- Ernest Hemingway, 1950.</p>
-          <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
-            <Col md={4}>
-              <Card style={{ padding: 10 }}>
-                <Container>
-                  <Card.Img style={{ padding: 10 }} src={Zurich} />
-                  <Card.Text>
-                    <i>Zürich | 苏黎世</i>
-                  </Card.Text>
-                </Container>
-              </Card>
-            </Col>
-            <Col md={4}>
-              <Card style={{ padding: 10 }}>
-                <Container>
-                  <Card.Img style={{ padding: 10 }} src={Luzern} />
-                  <Card.Text>
-                    <i>Lucerne | 卢塞恩</i>
-                  </Card.Text>
-                </Container>
-              </Card>
-            </Col>
-          </Row>
-          <br />
-          <br />
-          {/* <h4 style={{ textAlign: "left" }}>
+        </Row>
+        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+          <Col md={4}>
+            <Card style={{ padding: 10 }}>
+              <Container>
+                <Card.Img style={{ padding: 10 }} src={Zurich} />
+                <Card.Text>
+                  <i>Zürich</i>
+                </Card.Text>
+              </Container>
+            </Card>
+          </Col>
+          <Col md={4}>
+            <Card style={{ padding: 10 }}>
+              <Container>
+                <Card.Img style={{ padding: 10 }} src={Luzern} />
+                <Card.Text>
+                  <i>Lucerne</i>
+                </Card.Text>
+              </Container>
+            </Card>
+          </Col>
+          <Col md={4}>
+            <Card style={{ padding: 10 }}>
+              <Container>
+                <Card.Img style={{ padding: 10 }} src={Interlaken} />
+                <Card.Text>
+                  <i>Interlaken</i>
+                </Card.Text>
+              </Container>
+            </Card>
+          </Col>
+        </Row>
+        <br />
+        <br />
+        {/* <h4 style={{ textAlign: "left" }}>
             <b>Personal Moments</b>
           </h4>
           <hr></hr> */}
-        </Row>
       </Container>
     </Container>
   );
