bebe2cf4d623c0884526c65104ef48b2c195cb4e
chengzr01 <ziruicheng01@gmail.com>
2023-12-23 13:59:19 +0800

    add interest

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -94,12 +94,11 @@ function Home2() {
               <a href={"https://www.tsinghua.edu.cn/en/"}>
                 Tsinghua University
               </a>
-              . <br />
-              My research seeks to mitigate the gap between principles of
-              human-computer interaction (HCI) and realities of artificial
-              intelligence (AI) by researching the challenges in{" "}
-              <i>designing</i>, <i>developing</i>, and <i>evaluating</i>{" "}
-              AI-enabled systems.
+              . <br />I am interested in human-computer interaction and machine
+              learning. My research seeks to mitigate the gap between principles
+              of human-computer interaction and realities of artificial
+              intelligence by researching the challenges in <i>designing</i>,{" "}
+              <i>developing</i>, and <i>evaluating</i> AI-enabled systems.
               <br />
               During my undergraduate life, I have been fortunate to work with
               Prof.{" "}
