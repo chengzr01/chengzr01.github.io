536343aefcc001e06235cd4b08c89e371ddd7743
chengzr01 <ziruicheng01@gmail.com>
2024-09-29 10:54:43 -0500

    update photos

--- src/components/Page/Personal.js
+++ src/components/Page/Personal.js
@@ -15,76 +15,75 @@ function Personal() {
       id="about"
     >
       <Row>
-        <Col xs={4}>
+        <Col xs={4} style={{ display: "flex", justifyContent: "center" }}>
           <PhotoCard
-            width="300px"
-            height="200px"
+            width="360px"
+            height="240px"
             url={require("../../Assets/Misc/Vienna.jpg")}
             description="Vienna, Austria"
           />
         </Col>
-
-        <Col xs={4}>
+        <Col xs={4} style={{ display: "flex", justifyContent: "center" }}>
           <PhotoCard
-            width="300px"
-            height="200px"
+            width="360px"
+            height="240px"
             url={require("../../Assets/Misc/Salzburg.jpg")}
             description="Salzburg, Austria"
           />
         </Col>
-        <Col xs={4}>
+        <Col xs={4} style={{ display: "flex", justifyContent: "center" }}>
           <PhotoCard
-            width="300px"
-            height="200px"
+            width="360px"
+            height="240px"
             url={require("../../Assets/Misc/Munich.jpg")}
             description="Munich, Germany"
           />
         </Col>
-        <Col xs={4}>
+        <Col xs={4} style={{ display: "flex", justifyContent: "center" }}>
           <PhotoCard
-            width="300px"
-            height="200px"
+            width="360px"
+            height="240px"
             url={require("../../Assets/Misc/San Diego.jpg")}
             description="San Diego, USA"
           />
         </Col>
-        <Col xs={4}>
+        <Col xs={4} style={{ display: "flex", justifyContent: "center" }}>
           <PhotoCard
-            width="300px"
-            height="200px"
+            width="360px"
+            height="240px"
             url={require("../../Assets/Misc/Zurich.png")}
             description="Zurich, Switzerland"
           />
         </Col>
-        <Col xs={4}>
+        <Col xs={4} style={{ display: "flex", justifyContent: "center" }}>
           {" "}
           <PhotoCard
-            width="300px"
-            height="200px"
+            width="360px"
+            height="240px"
             url={require("../../Assets/Misc/Geneva.jpg")}
             description="Geneva, Switzerland"
           />
         </Col>
-        <Col xs={4}>
+        <Col xs={4} style={{ display: "flex", justifyContent: "center" }}>
           <PhotoCard
-            width="300px"
-            height="200px"
+            width="360px"
+            height="240px"
             url={require("../../Assets/Misc/Jeddah.jpg")}
             description="Jeddah, Saudi Arabia"
           />
         </Col>
-        <Col xs={4}>
+        <Col xs={4} style={{ display: "flex", justifyContent: "center" }}>
           <PhotoCard
-            width="300px"
-            height="200px"
+            width="360px"
+            height="240px"
             url={require("../../Assets/Misc/Suzhou.jpg")}
             description="Suzhou, China"
           />
         </Col>
-        <Col xs={4}>
+        <Col xs={4} style={{ display: "flex", justifyContent: "center" }}>
           <PhotoCard
-            width="300px"
-            height="200px"
+            width="360px"
+            height="240px"
             url={require("../../Assets/Misc/Hong Kong.jpg")}
             description="Hong Kong, China"
           />
