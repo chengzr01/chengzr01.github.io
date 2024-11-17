ceb60ce131c91f3a427721296eed11b841c90188
chengzr01 <ziruicheng01@gmail.com>
2024-11-16 22:21:12 -0600

    update photos

--- src/components/Page/Personal.js
+++ src/components/Page/Personal.js
@@ -15,6 +15,15 @@ function Personal() {
       id="about"
     >
       <Row>
+        <p
+          style={{
+            fontSize: "1.5em",
+            marginBottom: "0.5em",
+            marginTop: "0.5em",
+          }}
+        >
+          Photos
+        </p>
         <Col xs={4} style={{ display: "flex", justifyContent: "center" }}>
           <PhotoCard
             width="360px"
