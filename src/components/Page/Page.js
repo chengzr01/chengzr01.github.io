8ff33885ae90f18339bf00abea1a1d5e96654fe8
chengzr01 <ziruicheng01@gmail.com>
2025-07-16 07:55:23 -0400

    update page

--- src/components/Page/Page.js
+++ src/components/Page/Page.js
@@ -71,8 +71,9 @@ function Page() {
             always interested in making machine learning models more accessible
             and reliable so as to positively impact society. Currently, I am
             actively working on computational approaches (both theoretically and
-            empirically) to scalably understand the impacts of language models,
-            while also shedding light on problems in alignment and oversight.
+            empirically) to scalably understand the impacts of <i>strategic</i>{" "}
+            foundation models, while also shedding light on problems in
+            alignment and oversight.
             <br />
           </p>
           <div style={{ marginBottom: "0.5em", marginTop: "0.5em" }}></div>
